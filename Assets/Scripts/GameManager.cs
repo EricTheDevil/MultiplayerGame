9f08ef209904bebba08d53f5f699161301760846
Stefanos Edvard Kapsoritakis <stefan7@hotmail.gr>
2020-12-22 18:57:52 +0200

    Feat: Added board prototype, added gamemanager script
    Added a tiles movement on the board, gamemanager script instance

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager gameManagerInstance;
+    [SerializeField] public GameObject player;
+    public static GameManager instance
+    {
+        get
+        {
+            if (gameManagerInstance == null)
+            {
+                gameManagerInstance = FindObjectOfType<GameManager>();
+            }
+            return gameManagerInstance;
+        }
+    }
+}
---
+++ Assets/Scripts/Tiles.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiles : MonoBehaviour
+{
+    [SerializeField] List<Transform> tiles;
+    private GameObject player;
+    private Transform currentPlayerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameManager.instance.player;
+
+        player.transform.position = tiles[0].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                player.transform.position = Vector3.Lerp(tiles[i].transform.position, tiles[i + 1].transform.position, 5f);
+            }
+        }
+    }
+}
