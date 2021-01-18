61aae01b24cb3f35c154d7feb96a487ac95cc0f6
Eric <EricTheDevil@hotmail.com>
2021-01-18 04:32:15 +0200

    Feat: Prototype round system and updating the board.

---
+++ Assets/Scripts/RoundSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RoundState { START, WAITING, WON, LOST, PLAYERONE, PLAYERTWO }
+public class RoundSystem : MonoBehaviour
+{
+    public RoundState state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = RoundState.WAITING;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Tiles.cs
+++ Assets/Scripts/Tiles.cs
@@ -8,6 +8,7 @@ public class Tiles : MonoBehaviour
     private GameObject player;
     private Transform currentPlayerPos;
 
+    public float speed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,12 +20,19 @@ public class Tiles : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float step = speed * Time.deltaTime; // calculate distance to move
+        player.transform.position = Vector3.MoveTowards(player.transform.position, tiles[2].position, step);
+
         if (Input.GetKeyDown(KeyCode.P))
         {
+            Random.Range(-10.0f, 10.0f);
             for (int i = 0; i < 2; i++)
             {
-                player.transform.position = Vector3.Lerp(tiles[i].transform.position, tiles[i + 1].transform.position, 5f);
+                player.transform.position = Vector3.MoveTowards(player.transform.position, tiles[i].position, step);
+
+                //player.transform.position = Vector3.Lerp(tiles[i].transform.position, tiles[i + 1].transform.position, 10f);
             }
         }
     }
+
 }
