6c3ad6963e84c5b18179917ca14b297ac9b7618d
Stefanos Edvard Kapsoritakis <stefan7@hotmail.gr>
2021-02-11 21:08:12 +0200

    Unfinished name tags

---
+++ Assets/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,13 @@
+﻿using Bolt;
+using UnityEngine;
+
+public class NetworkCallbacks : GlobalEventListener
+{
+    public GameObject cubePrefab;
+
+    public override void SceneLoadLocalDone(string scene)
+    {
+        Vector3 spawnPos = new Vector3(0, 0.5f, 25);
+        BoltNetwork.Instantiate(cubePrefab, spawnPos, Quaternion.identity);
+    }
+}
