5f1c3a6b58796ba9a06473b95a117e45bf4832d8
Akseli <Akseli.Tontti@edu.turkuamk.fi>
2021-03-07 18:20:48 +0200

    minigametwo

---
+++ Assets/Scenes/Akseli/CoinCollcet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollcet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        DataHolder.Points += 1;
+        Destroy(gameObject);
+    }
+}
