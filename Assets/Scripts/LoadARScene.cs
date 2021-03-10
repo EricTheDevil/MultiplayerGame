a4a08d928d34d145c7a1b7e2f7242ac004efe9be
Eric <EricTheDevil@hotmail.com>
2021-03-10 05:20:14 +0200

    Stuff

---
+++ Assets/Scripts/LoadARScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadARScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void LoadAR()
+    {
+        SceneManager.LoadScene(3, LoadSceneMode.Single);
+    }
+    public void Unload()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
